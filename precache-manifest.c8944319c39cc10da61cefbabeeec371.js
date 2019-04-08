e31a27e590ad03e1e8e47d080dbeb91e584f0248
Helen Odia <helenodia@gmail.com>
2019-04-08 15:41:25 +0100

    Updates

---
+++ precache-manifest.c8944319c39cc10da61cefbabeeec371.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbd99f85eafe466b28b0",
+    "url": "/dreamteam-client/static/js/runtime~main.0948dc5f.js"
+  },
+  {
+    "revision": "acfe56a4398bc8bdc5b5",
+    "url": "/dreamteam-client/static/js/main.0d488d87.chunk.js"
+  },
+  {
+    "revision": "66b7623597a4e8db256b",
+    "url": "/dreamteam-client/static/js/2.63a4ac71.chunk.js"
+  },
+  {
+    "revision": "acfe56a4398bc8bdc5b5",
+    "url": "/dreamteam-client/static/css/main.7b3f5f9f.chunk.css"
+  },
+  {
+    "revision": "aa6c266356ffd4798069d03bb0fde331",
+    "url": "/dreamteam-client/index.html"
+  }
+];
